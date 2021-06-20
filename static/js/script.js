49268fe80695ae2ef4a9acade794b2266ef43750
sami-sinnari <samisinnari@gmail.com>
2021-06-20 15:47:27 +0000

    EmailJS functionality completed within script.js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "left"});
-  });
+
+function sendMail(reviewForm) {
+  emailjs.send("sami", "ms3-project", {
+      "from_name": reviewForm.name.value,
+      "from_email": reviewForm.email.value,
+      "request_message": reviewForm.message.value,
+  })
+      .then(
+          function () {
+              let sentButton = document.getElementById('send-btn');
+              sentButton.style.backgroundColor = "brown";
+              sentButton.innerHTML = "Review Sent";
+          },
+          function (error) {
+              alert("Ups, there's a problem! Please try submiting the review again! Thanks :)", error);
+          });
+          document.getElementById('reviewForm').reset();
+  return false;
+}
