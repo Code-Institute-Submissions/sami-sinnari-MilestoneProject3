e92b289f428bb13b67ade0c7459dca0b554b8fbe
sami-sinnari <samisinnari@gmail.com>
2021-06-26 18:14:37 +0000

    Add JS code for website functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let sidenavs = document.querySelectorAll(".sidenav");
+    let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "left"});
+    let collapsibles = document.querySelectorAll(".collapsible");
+    let collapsiblesInstance = M.Collapsible.init(collapsibles);
+    let tooltips = document.querySelectorAll(".tooltipped");
+    let tooltipsInstance = M.Tooltip.init(tooltips);
+    let selects = document.querySelectorAll("select");
+    let selectsInstance = M.FormSelect.init(selects);
 
-// function sendMail(reviewForm) {
-//   emailjs.send("sami", "ms3-project", {
-//       "from_name": reviewForm.name.value,
-//       "from_email": reviewForm.email.value,
-//       "request_message": reviewForm.message.value,
-//   })
-//       .then(
-//           function () {
-//               let sentButton = document.getElementById('send-btn');
-//               sentButton.style.backgroundColor = "brown";
-//               sentButton.innerHTML = "Review Sent";
-//           },
-//           function (error) {
-//               alert("Ups, there's a problem! Please try submiting the review again! Thanks :)", error);
-//           });
-//           document.getElementById('reviewForm').reset();
-//   return false;
-// }
+    validateMaterializeSelect();
+    function validateMaterializeSelect() {
+        let classValid = "border-bottom: 1px solid #4caf50; box-shadow: 0 1px 0 0 #4caf50;";
+        let classInvalid = "border-bottom: 1px solid #f44336; box-shadow: 0 1px 0 0 #f44336;";
+        let selectValidate = document.querySelector("select.validate");
+        let selectWrapperInput = document.querySelector(".select-wrapper input.select-dropdown");
+        if (selectValidate.hasAttribute("required")) {
+            selectValidate.style.cssText = "display: block; height: 0; padding: 0; width: 0; position: absolute;";
+        }
+        selectWrapperInput.addEventListener("focusin", (e) => {
+            e.target.parentNode.addEventListener("change", () => {
+                ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
+                for (let i = 0; i < ulSelectOptions.length; i++) {
+                    if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled") {
+                        e.target.style.cssText = classValid;
+                    }
+                }
+            });
+        });
+        selectWrapperInput.addEventListener("click", (e) => {
+            ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
+            for (let i = 0; i < ulSelectOptions.length; i++) {
+                if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled" && ulSelectOptions[i].style.backgroundColor == "rgba(0, 0, 0, 0.03)") {
+                    e.target.style.cssText = classValid;
+                } else {
+                    e.target.addEventListener("focusout", () => {
+                        if (e.target.parentNode.childNodes[3].hasAttribute("required")) {
+                            if (e.target.style.borderBottom != "1px solid rgb(76, 175, 80)") {
+                                e.target.style.cssText = classInvalid;
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+});
